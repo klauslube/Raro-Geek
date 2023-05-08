adb597345c359af05a5c635ae0b74dee18d63254
klauslube <klauslube93@gmail.com>
2023-05-08 15:25:26 -0300

    feat: add migration and model for supplier and client

---
+++ db/migrate/20230508175107_create_suppliers.rb
@@ -0,0 +1,11 @@
+class CreateSuppliers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :suppliers do |t|
+      t.references :address, null: false, foreign_key: true
+      t.string :name, null: false, limit: 255
+      t.string :CNPJ, null: false, limit: 20
+
+      t.timestamps
+    end
+  end
+end
