31bdb63c1f18a4966b7e7514959fd249f991ecfd
klauslube <klauslube93@gmail.com>
2023-05-09 12:43:24 -0300

    feat: add sale assoc

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,15 @@
 class Sale < ApplicationRecord
   belongs_to :client
   belongs_to :store
+
+  has_many :sale_products
+  has_many :products, through: :sale_products
+
+
+  enum :status, {
+    pending_payment: 1,
+    paid: 2,
+    overdue: 3,
+    cancelled: 4  
+  }, scopes: true, default: :pending_payment
 end
