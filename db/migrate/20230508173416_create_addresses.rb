36a08be13fe9e76636c7a9a1d8be8bfcdce2cb8c
klauslube <klauslube93@gmail.com>
2023-05-09 23:24:21 -0300

    refactor: change attribute name and order

--- db/migrate/20230508173416_create_addresses.rb
+++ db/migrate/20230508173416_create_addresses.rb
@@ -3,7 +3,7 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-      t.string :name, null: true, limit: 255
+      t.string :street, null: true, limit: 255
       t.string :public_place, null: false, limit: 255
       t.string :zip_code, null: false, limit: 8
       t.string :number, null: false, limit: 15
--- db/migrate/20230508180258_create_clients.rb
+++ db/migrate/20230508180258_create_clients.rb
@@ -3,11 +3,12 @@
 class CreateClients < ActiveRecord::Migration[7.0]
   def change
     create_table :clients do |t|
-      t.references :address, null: false, foreign_key: true
       t.string :name, null: false, limit: 255
-      t.string :CPF, null: false, limit: 15
+      t.string :CPF, null: false, limit: 15, unique: true
+      t.references :address, null: false, foreign_key: true
 
       t.timestamps
     end
   end
 end
+
