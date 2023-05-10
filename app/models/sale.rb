d94bbfaebbfd707742e896e451c9113589fd7590
klauslube <klauslube93@gmail.com>
2023-05-10 16:54:01 -0300

    feat: add callback to storage update

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -17,4 +17,24 @@ class Sale < ApplicationRecord
   validates :fiscal_number, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :total_amount, presence: true, numericality: { greater_than: 0 }
 
+  # after_validation :update_status
+
+  # def update_status
+  #   if sale_products.errors.empty?
+  #     update(status: 2)
+  #   else
+  #     update(status: 3)
+  #   end
+  # end
+
+  # after_save :update_storage
+
+  # def update_storage
+  #   sale_products.each do |sale_product|
+  #     storage = Storage.find_by(store_id: store_id, product_id: sale_product.product_id)
+  #     storage.update(quantity: storage.quantity - sale_product.quantity) if storage.present?
+  #   end
+  # end
+  
+
 end
