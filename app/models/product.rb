2dbebda479a00beb0a7f1558235f01e7d3dc652d
klauslube <klauslube93@gmail.com>
2023-05-10 02:45:56 -0300

    feat: add has many assoc to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   has_many :sales, dependent: :destroy
   has_many :product_suppliers, dependent: :destroy
   has_many :suppliers, through: :product_suppliers
-  has_many :products, through: :storages
+  has_many :stores, through: :storages
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :unit_price, presence: true, numericality: { greater_than: 0 }
