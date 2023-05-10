31db8a241a8e7066a79f4fcb13e5f04563d2c68c
klauslube <klauslube93@gmail.com>
2023-05-10 02:48:45 -0300

    feat: add has many assoc to product

--- app/models/sale_product.rb
+++ app/models/sale_product.rb
@@ -6,4 +6,24 @@ class SaleProduct < ApplicationRecord
 
   validates :quantity, presence: true
   validates :quantity, numericality: { only_integer: true, minimum: 1 }
+
+
+  scope :for_product_and_store, ->(product_id, store_id) { 
+    where(product_id: product_id, sale_id: Sale.where(store_id: store_id).pluck(:id))
+  }
+
+  validate :validate_quantity_for_sale_product
+
+  private
+
+  def validate_quantity_for_sale_product
+    if quantity > available_quantity
+      errors.add(:quantity, "is greater than the available quantity")
+    end
+  end
+
+  def available_quantity
+    Storage.for_product_and_store(product_id, sale.store_id).sum(:quantity)
+  end
+
 end
